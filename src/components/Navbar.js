e0628a708f91b3a64296cffe1d2ba5767ec972f4
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-24 02:57:00 +0900

    feat: navbar 삽입

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,17 @@ import './Navbar.css';
 function Navbar() {
 	return (
 		<nav className='navbar'>
-			<div className='navbar__logo'>Logo</div>
-			<ul className='navbar__menu'>
-				<li className='navbar__menu-item'>
-					<Link to='/'>Home</Link>
-				</li>
-				<li className='navbar__menu-item'>
-					<Link to='/movie'>Movie</Link>
-				</li>
-			</ul>
+			<div className='navbar__logo_menu'>
+				<div className='navbar__logo'>MinBox</div>
+				<ul className='navbar__menu'>
+					<li className='navbar__menu-item'>
+						<Link to='/'>Home</Link>
+					</li>
+					<li className='navbar__menu-item'>
+						<Link to='/movie'>Movie</Link>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 }
