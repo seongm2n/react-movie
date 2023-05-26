0e2c9e85f03b8722466aa280ef586f9b0946c1f6
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-26 17:09:22 +0900

    fix: styled-component로 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,70 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './Movie.css';
+import styled from 'styled-components';
+
+const MovieCover = styled.div`
+	width: 230px;
+	height: 345px;
+	margin: 10px 0px 0px 10px;
+	position: relative;
+	cursor: pointer;
+`;
+
+const MovieCard = styled.div`
+	&:hover {
+		opacity: 0.3;
+	}
+	&:hover div {
+		transition: opacity 0.3s ease;
+		opacity: 1;
+		background-color: #fee25b;
+		border-radius: 30px;
+	}
+`;
+const MovieImg = styled.div`
+	position: relative;
+`;
+
+const Img = styled.img`
+	display: block;
+	border-radius: 30px;
+`;
+
+const MovieData = styled.div`
+	width: 230px;
+	height: 345px;
+	display: flex;
+	transform: translate(0, -100%);
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	padding: 10px;
+	color: dark-gray;
+	opacity: 0;
+	position: absolute;
+	box-sizing: border-box;
+`;
+
+const MovieTitle = styled.h2`
+	margin: 20px;
+`;
 
 function Movie({ id, coverImg, title, description_full, genres, year }) {
 	return (
-		<div className="outside">
+		<MovieCover>
 			{/* <div className='movie__swipe'></div> */}
-			<div className='movie'>
-				<div className="movie__img">
-				<Link to={`/movie/${id}`}>
-					<img
-						src={coverImg}
-						alt={title}
-					/>
-				</Link>
-				</div>
-				<div className='movie__data'>
-					<h2 className='movie__title'>{title}</h2>
+			<MovieCard>
+				<MovieImg>
+					<Link to={`/movie/${id}`}>
+						<Img
+							src={coverImg}
+							alt={title}
+						/>
+					</Link>
+				</MovieImg>
+				<MovieData>
+					<MovieTitle className='movie__title'>{title}</MovieTitle>
 					<h3 className='movie__year'>{year}</h3>
 					<p className='movie__description'>{description_full}</p>
 					<ul className='genres'>
@@ -24,9 +72,9 @@ function Movie({ id, coverImg, title, description_full, genres, year }) {
 							<li key={g}>{g}</li>
 						))}
 					</ul>
-				</div>
-			</div>
-		</div>
+				</MovieData>
+			</MovieCard>
+		</MovieCover>
 	);
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+	position: fixed;
+	width: 100%;
+	/* font-family: var(--nav-font); */
+	justify-content: space-between;
+	background-color: rgba(149, 32, 80, 0.6);
+	align-items: center;
+	padding: 16px;
+	transition: all 300ms ease-in-out;
+	z-index: 11;
+`;
+const NavbarLogoMenu = styled.div`
+	display: flex;
+	text-align: center;
+`;
+const NavbarLogo = styled.div`
+	width: 220px;
+	height: 55px;
+	left: 16px;
+	top: 23px;
+	font-family: 'Inter';
+	font-style: italic;
+	font-weight: 900;
+	font-size: 50px;
+	line-height: 61px;
+	color: #fee25b;
+	text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+const NavbarMenu = styled.li`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	list-style: none;
+`;
+const NavbarMenuItem = styled.li`
+	padding: 8px 12px;
+	font-weight: 900;
+	font-size: 25px;
+	text-decoration: none;
+
+	&:hover {
+		background-color: #fee25b;
+		border-radius: 4px;
+	}
+`;
 
 function Navbar() {
 	return (
-		<nav className='navbar'>
-			<div className='navbar__logo_menu'>
-				<div className='navbar__logo'>MinBox</div>
-				<ul className='navbar__menu'>
-					<li className='navbar__menu-item'>
+		<Nav>
+			<NavbarLogoMenu>
+				<NavbarLogo>MinBox</NavbarLogo>
+				<NavbarMenu>
+					<NavbarMenuItem>
 						<Link to='/'>Home</Link>
-					</li>
-					<li className='navbar__menu-item'>
+					</NavbarMenuItem>
+					<NavbarMenuItem>
 						<Link to='/movie'>Movie</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
+					</NavbarMenuItem>
+				</NavbarMenu>
+			</NavbarLogoMenu>
+		</Nav>
 	);
 }
 
