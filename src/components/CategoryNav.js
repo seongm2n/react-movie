670b7b7ae3e38d3f93a68363ca2d317fb9eea04f
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-27 23:24:40 +0900

    create : 네브바에 category 추가

--- src/components/CategoryNav.js
+++ src/components/CategoryNav.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Nav = styled.nav`
 	position: fixed;
+	top: 0;
 	width: 100%;
 	justify-content: space-between;
 	background-color: rgba(149, 32, 80, 0.6);
@@ -15,6 +16,7 @@ const Nav = styled.nav`
 
 const NavbarLogoMenu = styled.div`
 	display: flex;
+	flex-direction: column;
 	text-align: center;
 `;
 
@@ -28,53 +30,130 @@ const NavbarLogo = styled.div`
 	font-weight: 900;
 	font-size: 50px;
 	line-height: 61px;
-	color: #fee25b;
-	text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
 
-const NavbarMenu = styled.li`
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	list-style: none;
-`;
-
-const NavbarMenuItem = styled.li`
-	padding: 8px 12px;
-	font-weight: 900;
-	font-size: 25px;
-	text-decoration: none;
-
-	&:hover {
-		background-color: #fee25b;
-		border-radius: 4px;
+	& a {
+		text-decoration: none;
+		color: #fee25b;
+		text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 	}
 `;
 
-const Category = styled.section`
+const NavbarMenu = styled.ul`
 	display: flex;
+	padding-left: 0;
+	align-items: center;
+	list-style: none;
+	flex-direction: row;
+	list-style: none;
+	margin-left: 24px;
 `;
 
-const NavPosition = styled.div`
-	display: flex;
-	flex-direction: column;
+const NavbarMenuItem = styled.li`
+	position: relative;
+	padding: 8px 12px;
+	font-weight: 900;
+	font-size: 25px;
+
+	& a {
+		text-decoration: none;
+		color: #fee25b;
+		text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+`;
+const CategoryList = styled.ul`
+	position: absolute;
+	top: 100%;
+	left: 0;
+	display: ${(props) => (props.isOpen ? 'block' : 'none')};
+	background-color: rgba(255, 255, 255, 0.8);
+	padding: 8px;
+	margin-top: 4px;
+	border-radius: 4px;
+	list-style: none;
+
+	& a {
+		display: block;
+		font-size: 18px;
+		padding: 4px 8px;
+		color: #333;
+		text-decoration: none;
+
+		&:hover {
+			background-color: #fee25b;
+			color: #fff;
+		}
+	}
 `;
 
 function Navbar() {
+	const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+	const handleCategoryClick = () => {
+		setIsCategoryOpen(!isCategoryOpen);
+	};
 	return (
 		<Nav>
 			<NavbarLogoMenu>
-				<NavbarLogo>MinBox</NavbarLogo>
+				<NavbarLogo>
+					<Link to={`/`}>MinBox</Link>
+				</NavbarLogo>
 				<NavbarMenu>
-					<NavPosition>
-						<NavbarMenuItem>
-							<Link to='/'>Home</Link>
-						</NavbarMenuItem>
-						<Category>
-							category
-							<option>dfdf</option>
-						</Category>
-					</NavPosition>
+					<NavbarMenuItem>
+						<Link to='/movie'>Movie</Link>
+					</NavbarMenuItem>
+					<NavbarMenuItem onClick={handleCategoryClick}>
+						<button
+							style={{
+								fontSize: '18px',
+								fontWeight: 900,
+								backgroundColor: 'rgba(255, 255, 255, 0.8)',
+								border: '1px solid yellow',
+								borderRadius: '5px',
+								cursor: 'pointer',
+								padding: '8px',
+							}}
+						>
+							CATEGORY
+						</button>
+						<CategoryList isOpen={isCategoryOpen}>
+							<li>
+								<Link to='/Documentary'>Documentary</Link>
+							</li>
+							<li>
+								<Link to='/Music'>Music</Link>
+							</li>
+
+							<li>
+								<Link to='/Thriller'>Thriller</Link>
+							</li>
+							<li>
+								<Link to='/Action'>Action</Link>
+							</li>
+
+							<li>
+								<Link to='/Drama'>Drama</Link>
+							</li>
+
+							<li>
+								<Link to='/History'>History</Link>
+							</li>
+							<li>
+								<Link to='/Sport'>Sport</Link>
+							</li>
+							<li>
+								<Link to='/Crime'>Crime</Link>
+							</li>
+							<li>
+								<Link to='/Mystery'>Mystery</Link>
+							</li>
+							<li>
+								<Link to='/Comedy'>Comedy</Link>
+							</li>
+						</CategoryList>
+					</NavbarMenuItem>
 				</NavbarMenu>
 			</NavbarLogoMenu>
 		</Nav>
